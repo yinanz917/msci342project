3558d65fcdf14952b412b1c91192e7f336bb0ebf
Vyomesh Iyengar <63476379+vyomeshiyengar@users.noreply.github.com>
2023-02-23 14:21:25 -0500

    Landing page changes

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -2,18 +2,33 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import NavBar from '../Navigation/NavBar';
+import Container from '@mui/material/Container';
+import houseImage from "../../img/house.jpg";
+import '../../index.css';
 
 const Landing = () => {
 
     return (
-        <Grid>
+        <Grid container direction="column">
             <NavBar/>
-            <Typography variant="h3" color="inherit" noWrap align='center'>
-                Welcome to Zoommates Landing Page!
-            </Typography>
+            <Grid container direction="row">
+                <Grid container direction="column">
+                    <Grid item sm>
+                    <Typography variant="h3" color="inherit" noWrap align='center' paddingTop={4}>
+                        <b>Welcome to Zoommates!</b>
+                    </Typography>
+                    </Grid>
+                    <Grid item sm>
+                    <Typography variant="h4" color="inherit" noWrap align='center' paddingTop={2}>
+                        Find your dream roommate!
+                    </Typography>
+                    </Grid>
+                </Grid>
+                <Container>
+                    <img height="100%" width="100%" src={houseImage} alt="An image of a house"/>
+                </Container>
+            </Grid>
         </Grid>
-
-
     );
 }
 export default Landing;
--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -4,4 +4,5 @@ export const SIGN_IN = '/signin';
 export const HOME = '/home';
 export const ACCOUNT = '/account';
 export const ADMIN = '/admin';
+export const STARRED = '/starred';
 export const PASSWORD_FORGET = '/pw-forget';
