f60752311330dbe74954624f19a13275f7048d50
“yinanz917” <yinanz917@gmail.com>
2023-02-23 17:25:26 -0500

    third update of profile

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import FormLabel from '@mui/material/FormLabel';
@@ -10,6 +11,13 @@ import InputAdornment from '@mui/material/InputAdornment';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
+import { MenuItem } from '@mui/material';
+import Select from '@mui/material/Select';
+import Chip from '@mui/material/Chip';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Slider from '@mui/material/Slider';
+import InputLabel from '@mui/material/InputLabel';
 
 import NavBar from '../Navigation/NavBar';
 
@@ -19,8 +27,15 @@ const Profile = () => {
   const [enteredAge, setAge] = React.useState('');
   const [selectedSex, setSex] = React.useState('');
   const [selectedPronouns, setPronouns] = React.useState('');
+
   const [enteredBudget, setBudget] = React.useState('');
   const [enteredLocation, setLocation] = React.useState('');
+
+  const [cleanLevel, setCleanLevel] = React.useState('');
+  const [noiseLevel, setNoiseLevel] = React.useState('');
+  const [hasPet, setHasPet] = React.useState('');
+  const [hobbyList, setHobbyList] = React.useState([]);
+
 
   const [submit, setSubmit] = React.useState(false);
 
@@ -44,11 +59,33 @@ const Profile = () => {
     setLocation(event.target.value);
   }
 
+  const handleCleanLevel = (event) => {
+    setCleanLevel(event.target.value);
+  }
+
+  const handleNoiseLevel = (event) => {
+    setNoiseLevel(event.target.value);
+  }
+
+  const handleHobbyList = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setHobbyList(
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+
+  const handlePetSelect = (event) => {
+    setHasPet(event.target.value);
+  }
+
 
   // TODO
-  // const handleSubmit = (event) => {
-  //   setSubmit(true);
-  // }
+  const handleSubmit = (event) => {
+    setSubmit(true);
+  }
 
   return (
     <div>
@@ -96,7 +133,6 @@ const Profile = () => {
         </Container>
       </Grid>
 
-
       <Grid marginTop={8}>
         <Container maxWidth="sm" margin={16}>
           <Typography variant="h5" color="inherit" noWrap align='left' paddingBottom={2}>
@@ -117,19 +153,6 @@ const Profile = () => {
                 userLocation={enteredLocation}
               />
             </Grid>
-
-            <Grid item id='date-picker'>
-              {/* <Dates>
-                onChange={handleDatePicker}
-                userDates={dateRange} 
-              </Dates> */}
-            </Grid>
-
-            <Grid item id='num-roomates'>
-
-            </Grid>
-
-
           </Grid>
         </Container>
       </Grid>
@@ -141,29 +164,48 @@ const Profile = () => {
           </Typography>
 
           <Grid container wrap="nowrap" spacing={4} direction="column">
-            <Grid item id='age'>
-
+            <Grid item id='cleanliness'>
+              <Cleanliness
+                onChange={handleCleanLevel}
+                userCleanLevel={cleanLevel}
+              />
             </Grid>
 
-            <Grid item id='age'>
-
+            <Grid item id='noise-level'>
+              <NoiseLevel
+                onChange={handleNoiseLevel}
+                userNoiseLevel={noiseLevel}
+              />
             </Grid>
 
-            <Grid item id='age'>
-
+            <Grid item id='pets'>
+              <Pets
+                onSelect={handlePetSelect}
+                userHasPet={hasPet}
+              />
             </Grid>
+
+            <Grid item id='hobbies'>
+              <Hobbies
+                onSelect={handleHobbyList}
+                userHobbies={hobbyList}
+              />
+            </Grid>
+
+
           </Grid>
         </Container>
       </Grid>
 
       <Typography>
-        {enteredAge}, {selectedPronouns}, {selectedSex}, {enteredBudget}
+        {enteredAge}, {selectedPronouns}, {selectedSex}, {enteredBudget}, {enteredLocation}, {cleanLevel}, {noiseLevel}, {hasPet}
+        {hobbyList}
       </Typography>
     </div >
 
   );
-}
 
+}
 
 const Age = (props) => {
   // const [errMessage, setErrMessage] = React.useState('');
@@ -191,14 +233,7 @@ const Age = (props) => {
 }
 
 const Sex = (props) => {
-  // const [errMessage, setErrMessage] = React.useState('');
   const sex = ['Female', 'Male', 'Other'];
-
-  // React.useEffect(() => {
-  //   if (props.isEmpty && props.submit) {
-  //     setErrMessage("Please select the rating.");
-  //   };
-  // }, [props.isEmpty, props.submit])
 
   return (
     <FormControl fullWidth>
@@ -295,27 +330,154 @@ const Location = (props) => {
   )
 }
 
-// const Dates = (props) => {
+const Cleanliness = (props) => {
 
-//   return (
-//     <LocalizationProvider
-//       dateAdapter={AdapterDayjs}
-//       localeText={{ start: 'Check-in', end: 'Check-out' }}
-//     >
-//       <DateRangePicker
-//         value={props.dateRange}
-//         onChange={props.onChange}
-//         renderInput={(startProps, endProps) => (
-//           <React.Fragment>
-//             <TextField {...startProps} />
-//             <Box sx={{ mx: 2 }}> to </Box>
-//             <TextField {...endProps} />
-//           </React.Fragment>
-//         )}
-//       />
-//     </LocalizationProvider>
-//   );
+  const levels = [
+    { value: 1, label: '1' },
+    { value: 2, label: '2' },
+    { value: 3, label: '3' },
+    { value: 4, label: '4' },
+    { value: 5, label: '5' },
+    { value: 6, label: '6' },
+    { value: 7, label: '7' },
+    { value: 8, label: '8' },
+    { value: 9, label: '9' },
+    { value: 10, label: '10' },
+  ];
 
-// }
+  return (
+    <div>
+      <FormControl fullWidth>
+        <FormLabel style={{ paddingBottom: 12 }}>How tidy are you with your space? Rank your level of cleanliness.</FormLabel>
+        <Stack spacing={4} direction="row" alignItems="center" paddingBottom={4}>
+          <Typography variant="body2" align="center">Not very clean</Typography>
+          <Slider
+            onChange={props.onChange}
+            defaultValue={5}
+            step={1}
+            min={1}
+            max={10}
+            marks={levels}
+          />
+          <Typography variant="body2" align="center">Extremely clean</Typography>
+        </Stack>
+      </FormControl>
+    </div>
+  );
+}
+
+const NoiseLevel = (props) => {
+
+  const levels = [
+    { value: 1, label: '1' },
+    { value: 2, label: '2' },
+    { value: 3, label: '3' },
+    { value: 4, label: '4' },
+    { value: 5, label: '5' },
+    { value: 6, label: '6' },
+    { value: 7, label: '7' },
+    { value: 8, label: '8' },
+    { value: 9, label: '9' },
+    { value: 10, label: '10' },
+  ];
+
+  return (
+    <div>
+      <FormControl fullWidth>
+        <FormLabel style={{ paddingBottom: 12 }}>How much noise do you typically make at home? Rank your level of noise.</FormLabel>
+        <Stack spacing={4} direction="row" alignItems="center">
+          <Typography variant="body2" align="center">Absolute silence</Typography>
+          <Slider
+            onChange={props.onChange}
+            defaultValue={5}
+            step={1}
+            min={1}
+            max={10}
+            marks={levels}
+          />
+          <Typography variant="body2" align="center">Constant noise</Typography>
+        </Stack>
+      </FormControl>
+    </div>
+  );
+}
+
+const Pets = (props) => {
+  const options = ['Yes', 'No'];
+
+  return (
+    <FormControl fullWidth>
+      <FormLabel>Do you have any pets?</FormLabel>
+      <RadioGroup
+        row
+        onChange={props.onSelect}
+      // error={(props.isEmpty && props.submit)}
+      >
+        {options.map((options) => (
+          <FormControlLabel value={options} control={<Radio color="primary" />} label={options} />
+        ))}
+      </RadioGroup>
+      {/* <FormHelperText>{(props.submit && props.isEmpty) && (errMessage)}</FormHelperText> */}
+    </FormControl>
+  )
+}
+
+const Hobbies = (props) => {
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+  const hobbies = [
+    'Hiking',
+    'Reading',
+    'Singing',
+    'Dancing',
+    'Sports',
+    'Gaming',
+    'Cooking',
+    'Writing',
+    'Travelling',
+    'Photography',
+  ];
+
+
+  return (
+    <div>
+      <FormControl fullWidth>
+        <FormLabel>Let us know some some of your hobbies are! Select as many as you want.</FormLabel>
+        <Select
+          id="demo-multiple-chip"
+          multiple
+          value={props.userHobbies}
+          onChange={props.onSelect}
+          input={<OutlinedInput id="select-multiple-chip" label="Select some hobbies" />}
+          renderValue={(selected) => (
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+              {selected.map((value) => (
+                <Chip key={value} label={value} />
+              ))}
+            </Box>
+          )}
+          MenuProps={MenuProps}
+        >
+          {hobbies.map((hobby) => (
+            <MenuItem
+              key={hobby}
+              value={hobby}
+            >
+              {hobby}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
 
 export default Profile;
