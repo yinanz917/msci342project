e88199270ac07907316de357076a0de7dad3109a
k29ferna <k29ferna@uwaterloo.ca>
2023-03-03 01:44:42 -0500

    Cypress and Jest Test

---
+++ client/src/components/Login/verifyLink.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const VerifyLink = () => {
+    return(
+        <div>
+            <Link to="/signup">Sign up</Link>
+        </div>
+    );
+}
+
+export default VerifyLink;
