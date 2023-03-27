e14aea0a8db927c545dcd2356b0980f01be2b98b
Vyomesh Iyengar <63476379+vyomeshiyengar@users.noreply.github.com>
2023-03-27 09:17:09 -0400

    fixed syntax

--- client/src/components/Chat/Search.js
+++ client/src/components/Chat/Search.js
@@ -43,14 +43,13 @@ const Search = () => {
             </Box>
             </div>
             {err && <span>User not found!</span>}
-            {user && <div className='userChat'>}
+            {user && <div className='userChat'>
                 <img className='searchImg' src='https://images.unsplash.com/photo-1568495341369-1066472c7a27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80'/>
                 <div className='userChatInfo'>
                     <span className='userChatInfo'>{user.first} + {user.last}</span>
                 </div>
             </div>}
-
         </div>
     )
-    };
-export default Search
+}
+export default Search;
